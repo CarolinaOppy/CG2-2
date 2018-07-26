8e885655a6fcdb00867622e65192b994da5dec7f
carocosta87 <carocosta87@gmail.com>
2018-07-25 20:17:21 -0500

    Added Code

--- CG2-2-A/Program.cs
+++ CG2-2-A/Program.cs
@@ -6,7 +6,15 @@ namespace CG2_2_A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("We are going to find the area of a rectangle.");
+            Console.Write("How many inches is the length of your rectangle? ");
+            int rectangleLength = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many inches is the height of your rectangle? ");
+            int rectangleHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your rectangle is " + rectangleHeight * rectangleLength + " square inches.");
+            Console.ReadLine();
         }
     }
 }
