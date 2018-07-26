439adc058f95789dac5d083756e742fda5bb8a71
carocosta87 <carocosta87@gmail.com>
2018-07-25 20:16:22 -0500

    Add project files.

---
+++ CG2-2-A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG2_2_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
